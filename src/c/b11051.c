dd9953b11f6ed35ebfb61600f667e023cc497833
dudgns <dudgns334@gmail.com>
2022-03-01 15:47:56 +0900

    2022.03.01

---
+++ src/c/b11051.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main() {
+    int n, k;
+    int arr[1001][1001] = {0};
+    scanf("%d %d", &n, &k);
+    for(int i = 0; i < 11; i++){
+        arr[i][0] = 1;
+        arr[i][i] = 1;
+    }
+    
+    for(int i = 1; i < 11; i++){
+        for(int j = 1; j < 11; j++){
+            arr[i][j] = (arr[i-1][j-1] + arr[i-1][j]) % 10007;
+        }
+    }
+    
+    printf("%d\n", arr[n][k]);
+}
