8034557d0e31add59e9c6b7b13741b9e381e1c42
dudgns <dudgns334@gmail.com>
2022-03-09 01:45:20 +0900

    2022.03.09

--- src/swexpertacademy/13038.c
+++ src/swexpertacademy/13038.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <math.h>
 int main(void)
 {
 	int test_case;
@@ -7,7 +8,40 @@ int main(void)
 	scanf("%d", &T);
 	for (test_case = 1; test_case <= T; ++test_case)
 	{
-        
+        int n, arr[7], weight = 0, min = 100000, p, temp;
+        scanf("%d\n", &n);
+        for(int i = 0; i < 7; i++){
+            scanf("%d", &arr[i]);
+        }
+        // for(int i = 0; i < 7; i++){
+        //     weight = 1000;
+        //     if(arr[i]){
+        //         weight = 1;
+        //         for(int j = 1; j < 7; j++){
+        //             if(arr[(i+j)%7]) weight += arr[(i+j)%7] * pow(2, j);
+        //         }
+        //     }
+        //     if(weight < min){
+        //         min = weight;
+        //         p = i;
+        //     }
+        // }
+        // answer = 0;
+        // while(n != 0){
+        //     if(arr[(p+answer)%7]) n--;
+        //     answer++;
+        // }
+        for(int i = 0; i < 7; i++){
+            if(arr[i]){
+                temp = n;
+                p = 0;
+                while(temp > 0){
+                    if(arr[(i+p++)%7]) temp--;
+                }
+                min = p < min ? p : min;
+            }
+        }
+        printf("#%d %d\n", test_case, min);
 	}
 	return 0; //정상종료시 반드시 0을 리턴해야 합니다.
 }
