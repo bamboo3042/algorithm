1558fe0d62bf039a6a3ac2308f53a0955aa53415
dudgns <dudgns334@gmail.com>
2022-03-15 22:57:39 +0900

    2022.03.15

---
+++ src/swexpertacademy/13550.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+unsigned a, d, n;
+unsigned *arr;
+unsigned mod = 1000003;
+unsigned temp;
+unsigned answer = 0;
+unsigned main(void)
+{
+	int test_case;
+	int T;
+	setbuf(stdout, NULL);
+	scanf("%d", &T);
+	for (test_case = 1; test_case <= T; ++test_case)
+	{
+        scanf("%u %u %u\n", &a, &d, &n);
+        arr = (unsigned*)malloc(sizeof(unsigned) * mod);
+        temp = 0;
+        arr[temp] = a;
+        do{
+            a = (a + d) % mod;
+            arr[temp+1] = (arr[temp] * a) % mod;
+            temp++;
+        }while(arr[temp] != arr[0] && n > temp);
+        printf("#%d %u\n", test_case, arr[temp == n ? n - 1 : n % temp]);
+        free(arr);
+	}
+	return 0; //정상종료시 반드시 0을 리턴해야 합니다.
+}
