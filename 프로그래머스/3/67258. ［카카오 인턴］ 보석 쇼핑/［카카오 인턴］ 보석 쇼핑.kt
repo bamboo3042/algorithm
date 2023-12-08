fc80153fc55836e43a3d5ff1a0de86b2acd4c4e7
bamboo3042 <39301206+bamboo3042@users.noreply.github.com>
2023-12-08 16:51:24 +0900

    [level 3] Title: [카카오 인턴] 보석 쇼핑, Time: 145.77 ms, Memory: 92.9 MB -BaekjoonHub

---
+++ 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.kt
@@ -0,0 +1,34 @@
+class Solution {
+    fun solution(gems: Array<String>): IntArray {
+        val answer = intArrayOf(1, gems.size)
+        var start = 0
+        var end = 0
+        val gemCount = gems.toSet().associateWith { 0 }.toMutableMap()
+        val set = mutableSetOf<String>()
+
+        while (end < gems.size) {
+            while (end < gems.size && set.size != gemCount.keys.size) {
+                gemCount[gems[end]] = gemCount[gems[end]]!! + 1
+                set.add(gems[end])
+                end++
+            }
+
+            while (start < gems.size && gemCount[gems[start]]!! > 1) {
+                gemCount[gems[start]] = gemCount[gems[start]]!! - 1
+                start++
+            }
+
+
+            if (set.size == gemCount.keys.size && end - (start+1) < answer[1] - answer[0]) {
+                answer[0] = start+1
+                answer[1] = end
+            }
+            
+            gemCount[gems[start]] = 0
+            set.remove(gems[start])
+            start++
+        }
+
+        return answer
+    }
+}
