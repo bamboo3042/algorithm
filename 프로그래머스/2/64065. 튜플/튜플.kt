a8c0d2239be9df097ff9c52aef6b84060c49a04a
bamboo3042 <39301206+bamboo3042@users.noreply.github.com>
2024-09-30 14:17:50 +0900

    [level 2] Title: 튜플, Time: 80.20 ms, Memory: 84.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/64065. 튜플/튜플.kt
@@ -0,0 +1,51 @@
+import java.util.PriorityQueue
+
+class Solution {
+    fun solution(s: String): IntArray {
+        val answer = mutableListOf<Int>()
+        val set = mutableSetOf<Int>()
+        val pq = stringToPq(s)
+
+        while (pq.isNotEmpty()) {
+            val temp = pq.poll()
+            
+            for (i in temp) {
+                if (!set.contains(i)) {
+                    answer.add(i)
+                    set.add(i)
+                    break
+                }
+            }
+        }
+
+        return answer.toIntArray()
+    }
+
+    fun stringToPq(s: String): PriorityQueue<Set<Int>> {
+        var n = 0
+        val list = mutableSetOf<Int>()
+        val pq = PriorityQueue<Set<Int>>() { o1, o2 -> o1.size - o2.size }
+
+        s.forEach { c ->
+            if (c == '{') return@forEach
+            if (c == '}') {
+                if (n == 0) return@forEach
+                list.add(n)
+                pq.add(list.toSet())
+                list.clear()
+                n = 0
+                return@forEach
+            }
+            if (c == ',') {
+                if (n == 0) return@forEach
+                list.add(n)
+                n = 0
+                return@forEach
+            }
+
+            n = n * 10 + c.code - '0'.code
+        }
+
+        return pq
+    }
+}
