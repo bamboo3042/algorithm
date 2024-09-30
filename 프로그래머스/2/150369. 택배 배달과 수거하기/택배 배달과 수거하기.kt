64d7d048981a77096ba9e1d47c063f4c60c17937
bamboo3042 <39301206+bamboo3042@users.noreply.github.com>
2024-09-30 15:50:00 +0900

    [level 2] Title: 택배 배달과 수거하기, Time: 22.33 ms, Memory: 68.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.kt
@@ -0,0 +1,58 @@
+class Solution {
+    fun solution(cap: Int, n: Int, deliveries: IntArray, pickups: IntArray): Long {
+        var answer: Long = 0
+
+        var dr = deliveries.indexOfLast { it != 0 }
+        var pr = pickups.indexOfLast { it != 0 }
+
+        while (dr != -1 && pr != -1) {
+            answer += (maxOf(dr, pr) + 1) * 2
+
+            dr = rMove(deliveries, cap, dr)
+            pr = rMove(pickups, cap, pr)
+        }
+
+        while (dr != -1) {
+            answer += (dr + 1) * 2
+            dr = rMove(deliveries, cap, dr)
+        }
+
+        while (pr != -1) {
+            answer += (pr + 1) * 2
+            pr = rMove(pickups, cap, pr)
+        }
+
+        return answer
+    }
+
+    fun rMove(array: IntArray, cap: Int, right: Int): Int {
+        var c = cap
+
+        for (i in right downTo 0) {
+            if (array[i] == 0) continue
+            if (array[i] > c) {
+                array[i] -= c
+                return i
+            }
+            else if (array[i] == c) {
+                array[i] = 0
+
+                return array.findZeroFromLast(i - 1)
+            }
+            else {
+                c -= array[i]
+                array[i] = 0
+            }
+        }
+
+        return -1
+    }
+
+    fun IntArray.findZeroFromLast(index: Int): Int {
+        for (i in index downTo 0) {
+            if (this[i] != 0) return i
+        }
+
+        return -1
+    }
+}
