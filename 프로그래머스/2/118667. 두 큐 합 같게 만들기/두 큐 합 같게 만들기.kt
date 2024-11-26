02e8ed54895b760f1e21d1d6c2be2cba9bfb2d9f
bamboo3042 <39301206+bamboo3042@users.noreply.github.com>
2024-11-26 16:49:03 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 24.54 ms, Memory: 104 MB -BaekjoonHub

---
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.kt
@@ -0,0 +1,34 @@
+class Solution {
+    fun solution(queue1: IntArray, queue2: IntArray): Int {
+        var halfSum = 0L
+        var sum = 0L
+
+        queue1.forEach {
+            halfSum += it
+            sum += it
+        }
+
+        queue2.forEach { halfSum += it }
+
+        halfSum /= 2
+
+        var point1 = 0
+        var point2 = 0
+        val maxMove = queue1.size + queue2.size + maxOf(queue1.size, queue2.size)
+
+        while (sum != halfSum && point1 + point2 <= maxMove) {
+            if (sum > halfSum) {
+                sum -= if (point1 >= queue1.size) queue2[point1 - queue1.size]
+                else queue1[point1]
+                point1++
+            }
+            else {
+                sum += if (point2 >= queue2.size) queue1[point2 - queue2.size]
+                else queue2[point2]
+                point2++
+            }
+        }
+
+        return if (sum == halfSum) point1 + point2 else -1
+    }
+}
