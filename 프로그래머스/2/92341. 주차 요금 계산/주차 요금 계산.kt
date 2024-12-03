9fe3c96f6b68340e5bdf9efff8f48b024ec2bd6a
bamboo3042 <39301206+bamboo3042@users.noreply.github.com>
2024-12-03 11:57:05 +0900

    [level 2] Title: 주차 요금 계산, Time: 29.00 ms, Memory: 68 MB -BaekjoonHub

---
+++ 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.kt
@@ -0,0 +1,30 @@
+import kotlin.math.ceil
+
+class Solution {
+    fun solution(fees: IntArray, records: Array<String>): IntArray {
+        val map = sortedMapOf<Int, MutableList<Int>>()
+        val endTime = 23 * 60 + 59
+
+        records.forEach { str ->
+            val (time, number, _) = str.split(" ")
+            val (h, m) = time.split(":")
+
+            map.getOrPut(number.toInt()) { mutableListOf() }.add(h.toInt() * 60 + m.toInt())
+        }
+
+
+        return map.map { (_, list) ->
+            var totalTime = -fees[0]
+            var totalPrice = fees[1]
+
+            for (i in 0 until list.size step 2) {
+                totalTime += if (i + 1 == list.size) endTime - list[i]
+                else list[i + 1] - list[i]
+            }
+
+            if (totalTime > 0) totalPrice += ceil(totalTime.toDouble() / fees[2]).toInt() * fees[3]
+
+            totalPrice
+        }.toIntArray()
+    }
+}
