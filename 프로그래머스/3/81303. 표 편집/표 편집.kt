3f9a41ba6efd472d160f808b0edb5a0ad40f1489
bamboo3042 <39301206+bamboo3042@users.noreply.github.com>
2024-12-18 15:57:55 +0900

    [level 3] Title: 표 편집, Time: 969.39 ms, Memory: 146 MB -BaekjoonHub

---
+++ 프로그래머스/3/81303. 표 편집/표 편집.kt
@@ -0,0 +1,72 @@
+import java.util.PriorityQueue
+import kotlin.math.absoluteValue
+
+class Solution {
+    fun solution(n: Int, k: Int, cmd: Array<String>): String {
+        val list = BooleanArray(n) { true }
+        var temp = k
+        val remove = ArrayDeque<Int>()
+        var moveCount = 0
+        val leftQueue = PriorityQueue<Int> { o1, o2 -> o2 - o1}
+        val rightQueue = PriorityQueue<Int>()
+
+        repeat(n) {
+            if (it < k) leftQueue.offer(it)
+            else if (it > k) rightQueue.offer(it)
+        }
+
+        fun moveFun() {
+            if (moveCount > 0) {
+                repeat(moveCount) {
+                    leftQueue.offer(temp)
+                    temp = rightQueue.poll()
+                }
+            }
+            else {
+                repeat(moveCount.absoluteValue) {
+                    rightQueue.offer(temp)
+                    temp = leftQueue.poll()
+                }
+            }
+
+            moveCount = 0
+        }
+
+        cmd.forEach { s ->
+            when(s[0]) {
+                'D' -> {
+                    val (_, ns) = s.split(" ")
+
+                    moveCount += ns.toInt()
+                }
+                'U' -> {
+                    val (_, ns) = s.split(" ")
+
+                    moveCount -= ns.toInt()
+                }
+                'C' -> {
+                    moveFun()
+
+                    list[temp] = false
+                    remove.addLast(temp)
+
+                    temp = if (rightQueue.isEmpty()) leftQueue.poll()
+                    else rightQueue.poll()
+                }
+                'Z' -> {
+                    moveFun()
+                    
+                    val recent = remove.removeLast()
+
+                    list[recent] = true
+
+                    if (recent < temp) leftQueue.offer(recent)
+                    else rightQueue.offer(recent)
+                }
+                else -> throw Exception()
+            }
+        }
+
+        return list.joinToString("") { if (it) "O" else "X" }
+    }
+}
