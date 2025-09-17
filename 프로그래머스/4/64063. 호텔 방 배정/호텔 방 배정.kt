26524452342e2363b0774fd02a3a7d9dd08cb91b
bamboo3042 <39301206+bamboo3042@users.noreply.github.com>
2025-09-17 14:56:47 +0900

    [level 4] Title: 호텔 방 배정, Time: 758.45 ms, Memory: 222 MB -BaekjoonHub

---
+++ 프로그래머스/4/64063. 호텔 방 배정/호텔 방 배정.kt
@@ -0,0 +1,27 @@
+class Solution {
+    fun solution(k: Long, roomNumber: LongArray): LongArray {
+        val map = mutableMapOf<Long, Long>()
+
+        return roomNumber.map { rn ->
+            if (map.contains(rn)) {
+                var temp = map[rn]!!
+                val visited = mutableListOf(rn)
+
+                while (map.contains(temp)) {
+                    visited.add(temp)
+                    temp = map[temp]!!
+                }
+
+                val next = temp + 1
+                map[temp] = next
+                visited.forEach { n -> map[n] = next }
+                temp
+            }
+            else {
+                map[rn] = rn + 1
+
+                rn
+            }
+        }.toLongArray()
+    }
+}
